6bbd1a5383edbde8c0fdc43e1dcf21648c3c0424
taranovpavel <155109498+taranovpavel@users.noreply.github.com>
2024-01-27 00:46:43 +0600

    11

--- js/script.js
+++ js/script.js
@@ -1,3 +1,58 @@
+const special = document.querySelector(".special_inner_main")
+const next = document.querySelector(".special_inner_right")
+const prev = document.querySelector(".special_inner_left")
+const name = document.querySelector(".name")
+const price = document.querySelector(".price")
+let i = 0
+const cardFetcher = (id) =>{
+	fetch(`../data/special.json`)
+		.then(response => response.json())
+		.then(data =>{
+			special.style.backgroundImage = `url(${data[id].dataImage})`
+			price.textContent = `${data[id].dataPrice}`
+			name.textContent = `${data[id].dataName}`
+		})
+}
+cardFetcher(i)
+const cardNext = () =>{
+	if (i >= 4){
+		i = 0
+		cardFetcher(i)
+	}else {
+		i++
+		cardFetcher(i)
+	}
+}
+next.onclick= () =>{
+	clearInterval(cardInterval)
+	cardNext()
+}
+prev.onclick = () =>{
+	clearInterval(cardInterval)
+	if (i <= 0){
+		i = 4
+		cardFetcher(i)
+	}else{
+		i--
+		cardFetcher(i)
+	}
+}
+
+const cardInterval = setInterval(() =>{
+	cardNext()
+},3000)
+
+
+const dataPosts = () =>{
+	fetch(`https://jsonplaceholder.typicode.com/posts`)
+		.then(response => response.json())
+		.then(data =>{
+			console.log(data)
+		})
+}
+dataPosts()
+
+
 $(document).ready(function(){
 	$('.slider').slick({
 		arrows:true,
@@ -22,4 +77,3 @@ $(document).ready(function(){
 		]
 	});
 });
-
